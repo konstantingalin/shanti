e7c2dc0e992a226645bbab1603a312396fc6633c
Konstantin <the.it.promo@gmail.com>
2023-12-04 02:35:55 +0600

    init

---
+++ lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ShantiApp extends StatelessWidget {
+  const ShantiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+    );
+  }
+}
